e8212b6d9b3e1acf4a52c4da6619c1d66dfac8bd
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-01-09 16:18:59 -0300

    Usando MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,10 +7,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Flutterando',
-        textDirection: TextDirection.rtl,
+    return const MaterialApp(
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text('Flutterando'),
       ),
     );
   }
