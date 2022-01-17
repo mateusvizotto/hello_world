0bb45d023fa2e7005eb3746237aab8ed3a08b9b1
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-01-17 15:11:19 -0300

    Componetizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,5 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage()
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Flutterando'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('Clicou');
-        },
-      ),
-    );
-  }
-}
