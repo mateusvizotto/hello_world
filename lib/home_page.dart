ccdc3a574ca0760274a1152265d59d65317268d3
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-01-19 16:55:32 -0300

    StatefulWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_controller.dart';
-
+/*
 class HomePage extends StatelessWidget {
   const HomePage({ Key? key }) : super(key: key);
 
@@ -22,6 +22,39 @@ class HomePage extends StatelessWidget {
         child: const Icon(Icons.add),
         onPressed: () {
           controller.increment();
+        },
+      ),
+    );
+  }
+}
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Text('Flutterando $counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
