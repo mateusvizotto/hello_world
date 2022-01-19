8c3ecca5bdeb47e1b43af4893557f285b8193d62
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2022-01-19 13:10:39 -0300

    InheritedWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var controller = HomeController.of(context);
+    final size = MediaQuery.of(context).size;
+    final navigation = Navigator.of(context);
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text('Flutterando'),
+      body: Center(
+        child: Text('Flutterando ${controller.value}'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          print('Clicou');
+          controller.increment();
         },
       ),
     );
